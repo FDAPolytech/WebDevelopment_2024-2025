babeb334128abd5cfa7623275e85ac2fc467d782
MartinAbdrakhmanov <113021702+MartinAbdrakhmanov@users.noreply.github.com>
2024-11-02 21:27:42 +0300

    oops

--- works/K3323/Абдрахманов_Мартин/lab2/gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-
-
-gulp.task('copy', () => {
-    return gulp.src('src_folder/*.txt')
-        .pipe(gulp.dest('dst_folder'));
-});
-
-gulp.task('default', gulp.series('copy'));
