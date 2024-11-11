a3ea14f8bee5dabca549ef6d144a1fc97f4ab8b5
NorthPole0499 <alekseev-timof@mail.ru>
2024-11-12 00:20:12 +0300

    Finished first task

--- works/K3321/Алексеев_Тимофей_Юрьевич/lab_3/gulp_tasks/gulpfile.js
+++ works/K3321/Алексеев_Тимофей_Юрьевич/lab_3/gulp_tasks/gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'); 
+const browserSync = require('browser-sync').create();
 
 gulp.task('hello', function(done) {
     console.log('Hello, my dear editor!');
@@ -12,3 +13,24 @@ gulp.task('bye', function(done) {
 
 gulp.task('orderTasks', gulp.series('hello', 'bye'));
 gulp.task('parallelTasks', gulp.parallel('hello', 'bye'));
+
+gulp.task('html', function () {
+    return gulp.src('app/index.html').pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('app/script.js').pipe(browserSync.stream());
+});
+
+gulp.task('server', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    });
+
+    gulp.watch('app/index.html',).on('change', browserSync.reload);
+    gulp.watch('app/script.js').on('change', browserSync.reload);
+});
+
+gulp.task('startWeb', gulp.series(gulp.parallel('html', 'scripts'), 'server'));
