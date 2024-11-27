6f896f1dbf7d7d6e860deaa54341327d84aaceb3
Nikiforov Saveliy <113004532+NikiforovSaveliy@users.noreply.github.com>
2024-11-27 18:23:25 +0300

    add ports)

--- works/K3320/Nikiforov Saveliy Denisovich/4/third/main.py
+++ works/K3320/Nikiforov Saveliy Denisovich/4/third/main.py
@@ -1,6 +1,7 @@
 import http.server
 import socketserver
 import os
+import sys
 
 def start_server(port):
     current_directory = os.getcwd()
@@ -11,5 +12,8 @@ def start_server(port):
         httpd.serve_forever()
 
 if __name__ == "__main__":
-    port = 1111
+    port = 888
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    print("serve in %s port" % port)
     start_server(port)
