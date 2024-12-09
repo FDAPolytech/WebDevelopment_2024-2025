f266ca9de3f64bf7add5cb76324a0e1e5e3ef54c
Alex-Nosov-ITMO <nosov3006@inbox.ru>
2024-12-09 13:35:18 +0300

    first commit lab2

---
+++ works/K3322/НосовАлександрДмитриевич/gulp/gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+
+const server = browserSync.create();
+
+gulp.task('serve', () => {
+  server.init({
+    server: {
+      baseDir: './',
+      index: 'ex.html'
+    },
+    port: 3000,
+    open: true
+  });
+
+  gulp.watch('ex.html').on('change', server.reload);
+});
+
+gulp.task('default', gulp.series('serve'));
