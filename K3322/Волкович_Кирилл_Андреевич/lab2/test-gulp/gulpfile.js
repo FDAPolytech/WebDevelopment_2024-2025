34978821a9e211e634408109aaa56ba72c36b677
kvolk04 <54364722+kvolk04@users.noreply.github.com>
2024-12-11 02:01:20 +0300

    laba3

--- K3322/Волкович_Кирилл_Андреевич/lab2/test-gulp/gulpfile.js
+++ K3322/Волкович_Кирилл_Андреевич/lab2/test-gulp/gulpfile.js
@@ -1,14 +1,22 @@
-// const gulp = require('gulp');
-
-// gulp.task('sum', (done) => {
-//   const a = 1;
-//   const b = a + 5;
-//   console.log(b + a);
-//   done();
-// });
-
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+
+gulp.task('sum', (done) => {
+  const a = 1;
+  const b = a + 5;
+  console.log(b + a);
+  done();
+});
+
+gulp.task('dif', (done) => {
+  const a = 8;
+  const b = 5;
+  console.log(a - b);
+  done();
+});
+
+gulp.task('seriesTasks', gulp.series('sum', 'dif'));
+gulp.task('parallelTasks', gulp.parallel('sum', 'dif'));
 
 const paths = {
   html: 'src/index.html',
