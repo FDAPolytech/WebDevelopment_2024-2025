59363bfe818b95b32c41d5cedcb7ffe160b1ffc6
dmitrijsq <107413710+dmitrijsq@users.noreply.github.com>
2024-12-13 16:29:46 +0300

    Update testserver.py

--- work/K3320/Сидненко Дмитрий Юрьевич/lab 4/testserver.py
+++ work/K3320/Сидненко Дмитрий Юрьевич/lab 4/testserver.py
@@ -3,17 +3,17 @@ import socketserver
 import os
 
 def run_server(port):
-    # Определяем адрес и порт
+    
     handler = http.server.SimpleHTTPRequestHandler
-    # Создаём сервер с указанным портом
+    
     with socketserver.TCPServer(("", port), handler) as httpd:
         print(f"Server running at http://127.0.0.1:{port}")
-        # Запуск сервера
+        
         httpd.serve_forever()
 
 if __name__ == "__main__":
-    port = 888  # Например, 888
-    # Убедитесь, что файл index.html находится в том же каталоге
+    port = 888  
+    
     if os.path.exists("index.html"):
         run_server(port)
     else:
