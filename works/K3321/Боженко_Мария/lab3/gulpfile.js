8bba7140faf87f5a7ec098dc1b69f94c68672675
Marie <masha_bojenko@mail.ru>
2024-12-18 15:00:30 +0300

    Added files for 2 part of 1 task and for 2 task

--- works/K3321/Боженко_Мария/lab3/gulpfile.js
+++ works/K3321/Боженко_Мария/lab3/gulpfile.js
@@ -17,3 +17,53 @@ const parallelTask = gulp.parallel(funcOne, funcTwo);
 exports.seriesTask = seriesTask;
 exports.parallelTask = parallelTask;
 
+// second part
+
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+
+const paths = {
+    styles: {
+        src: 'src/scss/**/*.scss',
+        dest: 'dist/css'
+    },
+    html: {
+        src: 'src/*.html',
+        dest: 'dist/'
+    }
+};
+
+function styles() {
+    return gulp.src(paths.styles.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream());
+}
+
+function html() {
+    return gulp.src(paths.html.src)
+        .pipe(gulp.dest(paths.html.dest))
+        .pipe(browserSync.stream());
+}
+
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        }
+    });
+
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.html.src, html);
+}
+
+exports.styles = styles;
+exports.html = html;
+exports.serve = serve;
+
+//const def = gulp.series(gulp.parallel(styles, html), serve);
+
+//exports.def = def;
+
+exports.default = gulp.series(gulp.parallel(styles, html), serve)
+
