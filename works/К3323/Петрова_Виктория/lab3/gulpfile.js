b83ca8e1e36e180d09e59b680953a3cfb83a2f55
notpetrusha <vika04vvp@mail.ru>
2024-12-25 14:03:12 +0300

    add task 2-3

--- works/К3323/Петрова_Виктория/lab3/gulpfile.js
+++ works/К3323/Петрова_Виктория/lab3/gulpfile.js
@@ -1,4 +1,6 @@
 const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const connectPHP = require('gulp-connect-php');
 
 // Пример первого таска
 gulp.task('task1', (done) => {
@@ -17,3 +19,28 @@ gulp.task('sequence', gulp.series('task1', 'task2'));
 
 // Параллельное выполнение
 gulp.task('parallel', gulp.parallel('task1', 'task2'));
+
+gulp.task('phpServer', function(done) {
+    connectPHP.server({
+        base: './',
+        port: 3000,
+        keepalive: true,
+        bin: 'D:/рамус/php/php.exe'
+    }, () => console.log('PHP server is running on http://127.0.0.1:3000'));
+    done();
+});
+
+gulp.task('browserSync', gulp.series('phpServer', function() {
+    browserSync.init({
+        proxy: '127.0.0.1:3000',
+        files: ['*/*.css', '*/*.html', '*.php'],
+        startPath: '/feedback.html'
+    });
+}));
+
+gulp.task('watch', gulp.series('browserSync', function() {
+    gulp.watch(['*/*.css', '*/*.html']).on('change', browserSync.reload);
+}));
+
+
+gulp.task('default', gulp.series('watch'));
