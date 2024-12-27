4c502f3e72be4225083995f7e2bdfb61f19723cb
warmike01 <51020399+warmike01@users.noreply.github.com>
2024-12-27 20:40:16 +0300

    gulp scripts

---
+++ tasks/lab3/gulpfile_2.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+gulp.task('one', (done) => {
+    console.log('Action 1');
+    done();
+   }); 
+   
+   gulp.task('two', (done) => {
+    console.log('Action 2');
+    done();
+   }); 
+function defaultTask(cb) 
+{
+  cb();
+}   
+gulp.task('default', gulp.series('one', 'two'));
+gulp.task('parallel', gulp.parallel('one', 'two'));
